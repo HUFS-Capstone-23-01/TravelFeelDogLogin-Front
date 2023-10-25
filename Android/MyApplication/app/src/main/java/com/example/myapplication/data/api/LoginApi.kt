ea64ff1e0c5eea994a23b1e437675ca32b4b3222
Yoon-Min <yoonmin0113@naver.com>
2023-10-25 15:57:15 +0900

    refactor: API 반환타입 수정

--- Android/MyApplication/app/src/main/java/com/example/myapplication/data/api/LoginApi.kt
+++ Android/MyApplication/app/src/main/java/com/example/myapplication/data/api/LoginApi.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.data.api
 import com.example.myapplication.data.model.ApiResponse
 import com.example.myapplication.data.model.TokenLoginRequest
 import com.example.myapplication.data.model.TokenLoginResponse
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,5 +12,5 @@ interface LoginApi {
     @POST("/login/mobile/oauth2/google/token")
     suspend fun mobileGoogleAuthenticationLogin(
         @Body request: TokenLoginRequest
-    ): Response<ApiResponse<TokenLoginResponse>>
+    ): Call<ApiResponse<TokenLoginResponse>>
 }
