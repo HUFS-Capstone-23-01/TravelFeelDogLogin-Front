070bf2510bc7ef5c769326e0bc7b0491540f57bd
Yoon-Min <yoonmin0113@naver.com>
2023-10-25 15:30:55 +0900

    delete: 프로젝트 구조 수정에 따른 일부 코드 삭제

--- Android/MyApplication/app/src/main/java/com/example/myapplication/TokenLoginRequest.kt
+++
@@ -1,4 +0,0 @@
-package com.example.myapplication
-
-data class TokenLoginRequest(val token: String)
-
--- Android/MyApplication/app/src/main/java/com/example/myapplication/TokenLoginResponse.kt
+++
@@ -1,4 +0,0 @@
-package com.example.myapplication
-
-data class TokenLoginResponse(val email: String,val type: String, val token: TokenResponse)
-
